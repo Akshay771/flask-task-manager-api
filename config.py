da76dc890ee7d337c8e1ad881236b889114081d3
“Akshay771’ <akshayrathod771@gmail.com>
2025-10-27 00:16:36 +0530

    env changes

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASS = os.getenv("DB_PASS", "postgres")
 DB_NAME = os.getenv("DB_NAME", "tasksdb")
 DB_HOST = os.getenv("DB_HOST", "pgdb")
+print(DB_HOST)
 
 SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}"
 # SQLALCHEMY_DATABASE_URI = "sqlite:///tasks.db"
